60641d71a1a1ff165a1ec5c7ca689f0a5f6c28aa
дом <@172.20.5.62>
2016-11-09 15:20:39 +0200

    rg

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author дом
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        // TODO code application logic here
+       
+        int date = 19;
+        int month = 03;
+        int year = 1993;
+        String irina = "Ирина";
+        String bogatenko = "Богатенко";
+        String nikolaevna = "Николаевна";
+
+// fullname = irina + "" + bogatenko + "" + nikolaevna;
+        System.out.println (bogatenko + " " + irina + " " + nikolaevna);
+       
+        System.out.println(date + " " + month + " " + year);
+        
+        
+    }
+    
+}
